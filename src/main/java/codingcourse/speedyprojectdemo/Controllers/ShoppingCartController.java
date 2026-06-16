dd45d41a6fe00b8da932d42068c6cd29e446f1ba
Jarito-arch <jaris.alday10@gmail.com>
2026-06-16 16:59:22 -0500

    Adding global navigation to LogoButton

--- src/main/java/codingcourse/speedyprojectdemo/Controllers/ShoppingCartController.java
+++ src/main/java/codingcourse/speedyprojectdemo/Controllers/ShoppingCartController.java
@@ -1,4 +1,24 @@
 package codingcourse.speedyprojectdemo.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ShoppingCartController {
+    @FXML
+    private void goToPrincipalMenu(MouseEvent event) throws IOException {
+        Parent root = FXMLLoader.load(
+                getClass().getResource("/codingcourse/speedyprojectdemo/PrincipalMenu.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource())
+                .getScene()
+                .getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
