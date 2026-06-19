ce1eb2cedc538f5829df48e2077606870a3a8128
Jarito-arch <jaris.alday10@gmail.com>
2026-06-19 17:14:57 -0500

    Register ver.01

--- src/main/java/SpeedyProject/Controllers/RegisterController.java
+++ src/main/java/SpeedyProject/Controllers/RegisterController.java
@@ -1,12 +1,16 @@
 package SpeedyProject.Controllers;
 
+import SpeedyProject.Dao.UserDao;
+import SpeedyProject.Dao.UserDaoImp;
+import SpeedyProject.Models.User;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
-
 import javafx.event.ActionEvent;
 import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -23,16 +27,50 @@ public class RegisterController {
         stage.show();
     }
     @FXML
-    private void goToPrincipalMenu(ActionEvent event) throws IOException{
-        Parent root = FXMLLoader.load(
-                getClass().getResource("/Views/PrincipalMenu.fxml"));
-        Stage stage = (Stage) ((Node) event.getSource())
-                .getScene()
-                .getWindow();
-        stage.setScene(new Scene(root));
-        stage.show();
+    private TextField txtname;
+    @FXML
+    private TextField txtEmail;
+    @FXML
+    private PasswordField txtPassword;
+    @FXML
+    private TextField txtConfirmPassword;
+
+    private UserDao dao = new UserDaoImp();
+
+    @FXML
+    private void register(ActionEvent event) throws IOException{
+        String name = txtname.getText();
+        String email = txtEmail.getText();
+        String password = txtPassword.getText();
+        String confirmPassword = txtConfirmPassword.getText();
+
+        //Validations to show on the views
+        if(name.isEmpty()|| email.isEmpty()||password.isEmpty()||confirmPassword.isEmpty()){
+            System.out.println("not enough data");
+            return;
+        }
+        if(!password.equals(confirmPassword)){
+            System.out.println("passwords are not equal");
+            return;
+        }
+        User newUser = new User();
+        newUser.setName(name);
+        newUser.setEmail(email);
+        newUser.setPassword(password);
+        boolean isRegistered = dao.register(newUser);
+        System.out.println("Registered successfully");
+
+        if(isRegistered){
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/PrincipalMenu.fxml"));
+            javafx.scene.Parent root = loader.load();
+            javafx.stage.Stage stage = (javafx.stage.Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+            javafx.scene.Scene scene = new javafx.scene.Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } else {
+            System.out.println("Failed to register in DB");
+        }
 
     }
-
 
 }
